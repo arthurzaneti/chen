22d339b1aa6148c61432a7ba3a006ef519708eda
Arthur Zaneti <afzaneti1@gmail.com>
2023-09-01 16:52:28 -0300

    Did some reorganizing in the rchen_ts function not working yet though. Wanna make it able to generate values with regression coefficients as well (and covariables)

--- R/rchen_ts.R
+++ R/rchen_ts.R
@@ -8,6 +8,7 @@
 #' @param freq Parameter for the time.series object that is returned
 #'
 #' @importFrom stats ts
+#' @import checkmate
 #' @return A time serie object with the generated values
 #' @export
 #'
@@ -16,7 +17,15 @@
 #' plot(rchen_ts(100, 1, 0.7, ma_coef = c(0.5, 0.2)))
 #' rchen_ts(100, 1, 0.7, ar_coef = c(0.5, 0.3), ma_coef = c(0.3, 0.1))
 #'
-rchen_ts <- function(n, intercept, lambda, ar_coef = NULL, ma_coef = NULL, freq = 1) {
+rchen_ts <- function(n, intercept, lambda, ar_coef = NULL, ma_coef = NULL, reg_coef = NULL, cvar = NULL, freq = 1) {
+  tryCatch(cvar <- as.matrix(cvar) , error = function(e) stop("The value sent for cvar is not coercible to matrix"))
+  checkmate::assert_integerish(n, lower = 1)
+  checkmate::assert_number(lambda, lower = 0)
+  checkmate::assert_numeric(ar_coef, null.ok = T)
+  checkmate::assert_numeric(ma_coef, null.ok = T)
+  checkmate::assert_numeric(reg_coef, null.ok = T)
+  checkmate::assert_integerish(freq, lower = 1)
+
   #______________________________________ORGANIZING_______________________________________
   if(!is.null(ar_coef)) {
     ar <- 1:length(ar_coef)
@@ -27,12 +36,23 @@ rchen_ts <- function(n, intercept, lambda, ar_coef = NULL, ma_coef = NULL, freq 
     q <- length(ma)
   }
 
-  if(!is.null(ar_coef) && !is.null(ma_coef)) case <- "ARMA"
-  else if(!is.null(ar_coef)) case <- "AR"
-  else if(!is.null(ma_coef)) case <- "MA"
-  else{
-    stop("No values for ar_coef or ma_coef provided")
+  if(is.null(reg_coef)){
+    if(!is.null(ar_coef) && !is.null(ma_coef)) case <- "ARMA"
+    else if(!is.null(ar_coef)) case <- "AR"
+    else if(!is.null(ma_coef)) case <- "MA"
+    else{
+      stop("No values for ar_coef or ma_coef provided")
+    }
+
+  }else{
+    if(!is.null(ar_coef) && !is.null(ma_coef)) case <- "REG_ARMA"
+    else if(!is.null(ar_coef)) case <- "REG_AR"
+    else if(!is.null(ma_coef)) case <- "REG_MA"
+    else{
+      stop("No values for ar_coef or ma_coef provided")
+    }
   }
+
   buffer <- 50
   #_________________________________________ARMA____________________________________________
   if(case == "ARMA"){
@@ -79,7 +99,7 @@ rchen_ts <- function(n, intercept, lambda, ar_coef = NULL, ma_coef = NULL, freq 
   }
 
   #_________________________________________MA____________________________________________
-  else {
+  else if(case == "MA") {
 
     ynew <-rep(intercept, (n + buffer))
     mu <- exp(ynew)
@@ -96,6 +116,26 @@ rchen_ts <- function(n, intercept, lambda, ar_coef = NULL, ma_coef = NULL, freq 
       error[i]<- ynew[i] - eta[i]
     }
     return(stats::ts(y[(buffer + 1):(n + buffer)], frequency = freq) )
+  }
+  #______________________________________REG ARMA_________________________________________
+  if(case == "REG_ARMA"){
+
+    maxx = max(p, q)
+
+    ynew <-rep(intercept, (n + buffer))
+    mu <- exp(ynew)
+
+    error <- rep(0, n + buffer) # E(error)=0
+    eta <- y <- NULL
+    for(i in (maxx + 1):(n + buffer))
+    {
+      eta[i] <- intercept + (ar_coef %*% (ynew[i - ar])) + (ma_coef %*% error[i - ma])
+      mu[i] <- exp(eta[i])
+      y[i] <- chen::rchen_rpr(1, c(lambda, mu[i]))
+      ynew[i] <- log(y[i])
+      error[i] <- ynew[i] - eta[i]
+    }
+    return(stats::ts(y[(buffer + 1) : (n + buffer)], frequency = freq) )
   }
 }
 
