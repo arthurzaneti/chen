e08910aa3b241f0d475003081bd8f67dbe9beb97
Arthur Zaneti <afzaneti1@gmail.com>
2023-09-04 15:36:58 -0300

    Not working yet, but reg_chen_ts will work soon

--- R/reg_chen_ts.R
+++ R/reg_chen_ts.R
@@ -15,7 +15,7 @@ reg_chen_ts <- function(y, ar = NULL, ma = NULL, cvar = NULL, tau = 0.5){
   if(!any(is.null(cvar))) cvar <- as.matrix(cvar)
   #__________________________________ORGANIZING____________________________________
 
-  log_y <- log(y)
+  # log_y <- log(y)
 
   isar <- !chen::is_null(ar)
   isma <- !chen::is_null(ma)
@@ -23,83 +23,108 @@ reg_chen_ts <- function(y, ar = NULL, ma = NULL, cvar = NULL, tau = 0.5){
 
   case <- chen::arma_case(isar, isma, isreg)
 
-  if(isma && isar) {
-    max_arma <- max(max_ma <- max(ma), max_ar <- max(ar))
-  } else if(isar) {
-    max_arma <- max_ar <- max(ar)
-  } else if(isma) {
-    max_arma <- max_ma <- max(ma)
-    print(max_arma)
-  }
+  return(switch(case,
+                "ARMA" = ARMA(y, ar, ma, tau), #max_ar, max_ma, ar, ma, max_arma, tau, mqo,
+                "AR" = AR(),
+                "MA" = MA(),
+                "REG_ARMA" = REG_ARMA(),
+                "REG_AR" = REG_AR(),
+                "REG_MA" = REG_MA()))
 
+#   if(isma && isar) {
+#     max_arma <- max(max_ma <- max(ma), max_ar <- max(ar))
+#   } else if(isar) {
+#     max_arma <- max_ar <- max(ar)
+#   } else if(isma) {
+#     max_arma <- max_ma <- max(ma)
+#     print(max_arma)
+#   }
+#   n <- length(y)
+#   n_fit <- n - max_arma
+#
+#   y_cut<- y[(max_arma + 1): n]
+#   log_y_cut <- log_y[(max_arma + 1): n]
+#
+#   if(isar){
+#     n_ar <- length(ar)
+#     mat <-matrix(rep(NA, n_fit * max_ar), ncol = n_ar)
+#     indices <- 1:n_fit
+#     for(i in ar){
+#       mat[indices, i] <- log_y[indices + max_arma - i]
+#     }
+#     mat <- cbind(rep(1, n_fit), mat)
+#   }else{
+#     mat <- as.matrix(rep(1, n_fit))
+#   }
+#
+#   if(isma){
+#     n_ma <- length(ma)
+#   }
+#
+#   if(!isreg){
+#     # Starting parameters
+#     opt <- stats::lm.fit(mat, log_y_cut)
+#     mqo <- stats::coef(opt)
+#     n_par <- length(mqo)
+#
+#   } else {
+#
+#     x <- cbind(mat, cvar[(max_arma + 1) : n, ])
+#     opt <- stats::lm.fit(mat, log_y_cut)
+#     mqo <- stats::coef(opt)
+#     n_par <- length(mqo)
+#
+#     dlog <- 1/exp(stats::fitted(opt))
+#     err <- stats::residuals(opt)
+#   }
+#
+# }
+}
+#____________________________________CASES________________________________________
+ARMA <- function(y, ar, ma, tau){
+  log_y <- log(y)
+  max_arma <- max(max_ma <- max(ma), max_ar <- max(ar))
   n <- length(y)
   n_fit <- n - max_arma
-
+  n_ar <- length(ar)
+  n_ma <- length(ma)
   y_cut<- y[(max_arma + 1): n]
   log_y_cut <- log_y[(max_arma + 1): n]
 
-  y_prev <- c(rep(NA, n))
-
-  if(isar){
-    n_ar <- length(ar)
-    mat <-matrix(rep(NA, n_fit * n_ar), ncol = n_ar)
-    indices <- 1:n_fit
-    for(i in ar){
-      mat[indices, i] <- log_y[indices + max_arma - i]
-    }
-    mat <- cbind(rep(1, n_fit), mat)
-  }else{
-    mat <- as.matrix(rep(1, n_fit))
+  mat <- matrix(rep(NA, n_fit * n_ar), ncol = n_ar)
+  indices <- 1:n_fit
+  for(i in 1:length(ar)){
+    mat[indices, i] <- log_y[indices + max_arma - ar[i]]
   }
 
-  if(isreg){
-    # Starting parameters
-    opt <- stats::lm.fit(mat, log_y_cut)
-    start <- stats::coef(opt)
-    n_par <- length(start)
-    lambda <- 0.7
+  mat <- cbind(rep(1, n_fit), mat)
+  opt <- stats::lm.fit(mat, log_y_cut)
+  mqo <- stats::coef(opt)
+  n_par <- length(mqo)
 
-  } else {
+  #___________________________________
+  names_phi <- c(paste("phi", ar, sep = ""))
+  names_rho <- c(paste("rho", ma, sep = ""))
+  names_par <- c("intercept", names_phi, "lambda")
 
-    x <- cbind(mat, cvar[(max_arma + 1) : n, ])
-    opt <- stats::lm.fit(mat, log_y_cut)
-    start <- stats::coef(opt)
-    n_par <- length(start)
-    lambda <- 0.7
-
-    dlog <- 1/exp(stats::fitted(opt))
-    err <- stats::residuals(opt)
-  }
-  lambda <- 0.7
-  #_______________________________________________________________________________
-  switch(case,
-         "ARMA" = ARMA(),
-         "AR" = AR(),
-         "MA" = MA(),
-         "REG_ARMA" = REG_ARMA(),
-         "REG_AR" = REG_AR(),
-         "REG_MA" = REG_MA())
-}
-
-ARMA <- function(lambda, start, y, n, n_fit, y_cut, log_y, log_y_cut, max_arma, max_ar, max_ma){
-
-  names_phi<-c(paste("phi",ar,sep=""))
-  names_par <- c("beta0",names_phi,"lambda")
-
-  opt <- stats::optim(c(start, lambda),
+  opt <- stats::optim(c(mqo, rep(0, n_ma), 0.7),
                       chen::ll_ARMA,
+                      y = y, y_cut = y_cut, log_y = log_y, n = n,
+                      n_ar = n_ar, n_ma = n_ma, ar = ar,
+                      ma = ma, max_arma = max_arma, tau = tau,
                       hessian = T,
                       method = "BFGS",
                       control = list(fnscale = -1))
-
+  print(mqo)
+  print(opt$par)
   model <- list()
-  coef <-c(opt$par)[1:(n_ar+2)]
+  coef <-c(opt$par)[1:(max_ar + max_ma)]
   names(coef) <- names_par
   model$coef <- coef
   model$beta0 <- beta0 <- coef[1]
-  model$phi <- phi <- coef[2:(n_ar + 1)]
-  model$rho <- rho <- coef[(n_ar + 2): (n_ar + n_ma + 1)]
-  model$lambda <- lambda <- coef[(n_ar + n_ma + 2)]
+  model$phi <- phi <- coef[2:(max_ar + 1)]
+  model$rho <- rho <- coef[(max_ar + 2): (max_ar + max_ma + 1)]
+  model$lambda <- lambda <- coef[(max_ar + max_ma + 2)]
   model$hessian <- opt$hessian
   model$fitted <- c(rep(NA, max_arma), y_cut)
 
@@ -110,10 +135,12 @@ ARMA <- function(lambda, start, y, n, n_fit, y_cut, log_y, log_y_cut, max_arma, 
     etahat[i] <- beta0 + (phi %*% y_cut[i - ar]) + (rho %*% errorhat[i - ma])
     errorhat[i] <- y_cut[i] - etahat[i]
   }
-  model$fitted <- ts(c(rep(NA, m), muhat), start = start(y), frequency = frequency(y))
+  muhat <- exp(etahat[(max_arma + 1):n])
+  model$fitted <- ts(c(rep(NA, max_arma), muhat), start = start(y), frequency = frequency(y))
   model$etahat <- etahat
   model$errorhat <- errorhat
-
+  class(model) <- "CHARMA_reg"
+  return(model)
 }
 AR <- function(){
 
@@ -132,7 +159,6 @@ REG_MA <- function(){
 }
 
 
-#test
 
 
 
--- R/reg_chen_ts_utils.R
+++ R/reg_chen_ts_utils.R
@@ -1,5 +1,25 @@
 #' Title
 #'
+#' @param y
+#' @param theta
+#' @param tau
+#'
+#' @return
+#' @keywords internal
+#' @export
+#'
+#' @examples
+pdf_chen_rpr_ts <- function(y, theta, tau){
+  #__________________________________end_checks_________________________________
+
+  lambda <- theta[1]
+  mu <- theta[2 : length(theta)]
+  return((log(1 - tau) / (1 - exp(mu ^ lambda))) * lambda * y ^ (lambda - 1) *
+           exp((log(1 - tau) / (1 - exp(mu ^ lambda))) * (1 - exp(y ^ lambda)) + y ^ lambda))
+}
+
+#' Title
+#'
 #' @param isma
 #' @param isar
 #' @param isreg
@@ -34,8 +54,8 @@ arma_case <- function(isar, isma, isreg){
 #' @param log_y
 #' @param theta
 #' @param n
-#' @param n_ar
-#' @param n_ma
+#' @param max_ar
+#' @param max_ma
 #' @param ar
 #' @param ma
 #' @param max_arma
@@ -46,19 +66,18 @@ arma_case <- function(isar, isma, isreg){
 #'
 #' @examples
 
-ll_ARMA <- function(y, y_cut, log_y, theta, n, n_ar, n_ma, ar, ma, max_arma){
+ll_ARMA <- function(y, y_cut, log_y, theta, n, n_ar, n_ma, ar, ma, max_arma, tau){
   beta0 <- theta[1]
   phi <- theta[2:(n_ar + 1)]
   rho <- theta[(n_ar + 2):(n_ar + n_ma + 1)]
-  lambda <- theta[n_ar + n_ma + 2]
-
+  lambda <- theta[n_ar +n_ma + 2]
   error <- eta <- numeric(n)
 
   for(i in (max_arma + 1):n) { # Don't really know if we can avoid the for loop in here, I use the indice in to many places
-    eta[i] <- beta0 + (phi%*%ynew[i - ar]) + (rho%*%error[i - ma])
-    error[i] <- ynew[i] - eta[i]
+    eta[i] <- beta0 + (phi%*%log_y[i - ar]) + (rho%*%error[i - ma])
+    error[i] <- log_y[i] - eta[i]
   }
 
   mus <- exp(eta[(max_arma + 1) : n])
-  sum(chen::pdf_chen_rpr(y_cut, c(lambda, mus), tau))
+  sum(chen::pdf_chen_rpr_ts(y_cut, c(lambda, mus), tau))
 }
